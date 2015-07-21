220f7ba21e3432292803051e9facc09de9e90cbb
emanuelbatista <emanuelbatista2011@gmail.com>
2015-07-21 11:36:08 -0300

    adicionando entidade Usuário e o DAO de Usuário

---
+++ blogsoon-core/src/main/java/br/edu/ifpb/bdnc/blogsoon/entidades/Usuario.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.bdnc.blogsoon.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author Emanuel Batista da Silva Filho - emanuelbatista2011@gmail.com
+ */
+@Entity
+public class Usuario implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotEmpty(message = "O nome está vazio")
+    private String nome;
+    private String sobrenome;
+    @NotEmpty(message = "O login está vazio")
+    private String login;
+    @NotEmpty(message = "A senha está vazia")
+    private String senha; 
+    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
